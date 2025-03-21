8d25c298a3017f855b7715d4f4c5cdb77c4e7ecb
Lander <lander.nieto@hispavista.com>
2025-03-21 11:30:44 +0100

    added os xD

--- DASH_PYTHON.py
+++ DASH_PYTHON.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,4 +9,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
-
